e979bce882ee04f1ed4b1df732facf65a5d76767
Fred-S <fredvantage@gmail.com>
2021-01-29 11:44:17 +0100

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import { Button, Alert } from 'reactstrap'
+
+export default () => {
+    return (
+        <div>
+            <div>
+                <Alert color='primary'>Hello From Food Delivry</Alert>
+                &nbsp; <Button color='primary'>Hello from nextjs</Button>
+            </div>
+        </div>
+    )
+}
